13a983ab030cdc1b19c3681a8f1318a6455486a2
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-05-29 13:21:45 +0545

    jsx and library flowings

--- 01vitereact/src/New.jsx
+++ 01vitereact/src/New.jsx
@@ -1,5 +1,6 @@
 function neww(params) {
-    return <h3>New file is ready to serve</h3>
+    const userName = 'Basanta'
+    return <h3>New file is ready to serve {userName}</h3>
 }
 
 export default neww
