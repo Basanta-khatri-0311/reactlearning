0ec2d3eadbf712e4a00e57ceb601ddc7519479eb
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-05-29 13:54:34 +0545

    how usestate works with the variable changes and why react was used

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,15 +4,24 @@ function App() {
   let [counter, setCounter] = useState(0);
   const addValue = () => {
     setCounter(counter + 1);
-    // console.log("Value added", counter);
+  };
+  const removeValue = () => {
+    // if (counter <= 0 ) {
+    //   return;
+    // } else {
+    // }
+    setCounter(counter - 1);
   };
   return (
     <>
       <h1>Hello From react learning | Counter</h1>
       <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add value {counter}</button>
+
+      <button onClick={addValue} disabled = {counter===20}>Add value {counter}</button>
+
       <br />
-      <button>Remove value</button>
+
+      <button onClick={removeValue} disabled ={counter === 0}>Remove value {counter}</button>
     </>
   );
 }
