6413a4cb24df56d867aa9953f9c6f0492ae8db33
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-05-29 12:28:24 +0545

    learning how react runs and renders the components

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// function NewApp() {
+//   return <h1>My custom App!!!</h1>
+// }
+
+const newElement = <a href="httsp://www.google.com">Google</a>;
+
+const anotherElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Ok Google!!!"
+);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    {/* <NewApp /> */}
+   
+  </StrictMode>
+);
