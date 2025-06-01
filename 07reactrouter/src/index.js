a63ffdff27cd536029bb7aae57643e798e46e337
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-06-01 12:29:22 +0545

    learning different ways of creating a routes in react using react-router-dom

---
+++ 07reactrouter/src/index.js
@@ -0,0 +1,7 @@
+// import {Header} from './components/Header/Header'
+// import {Footer} from './components/Footer/Footer'
+// import {Contact} from './components/Contact/Contact'
+// import {About} from './components/About/About'
+
+
+// export default [Header,Footer,Contact,About];
