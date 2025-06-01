2474d4f4adce4109d0e0fdeac6e0329449da4c7e
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-06-01 13:42:53 +0545

    routes with loader

--- 07reactrouter/src/components/GitHub/Github.jsx
+++ 07reactrouter/src/components/GitHub/Github.jsx
@@ -1,25 +1,33 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data,setData] = useState([])
-    useEffect(() => {
-        const fetchApi = async () => {
-            try {
-                const data = await fetch('https://api.github.com/users/Basanta-khatri-0311')
-                const formatedData =await data.json()
-                setData(formatedData)
-            } catch (error) {
-                console.log(`Error while fetching data: ${error}`)
-            }
-        }
-        fetchApi()
-    }, [])
-    
+    // const [data,setData] = useState([])
+    // useEffect(() => {
+    //     const fetchApi = async () => {
+    //         try {
+    //             const data = await fetch('https://api.github.com/users/Basanta-khatri-0311')
+    //             const formatedData =await data.json()
+    //             setData(formatedData)
+    //         } catch (error) {
+    //             console.log(`Error while fetching data: ${error}`)
+    //         }
+    //     }
+    //     fetchApi()
+    // }, [])
+    const data = useLoaderData()
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl '>
       GitHub Followers: {data.followers}
+      <img src={data.avatar_url} alt="git picture" width={300}/>
     </div>
   )
 }
 
 export default Github
+
+
+export const githubLoader = async  () => {
+    const resp = await fetch('https://api.github.com/users/Basanta-khatri-0311')
+    return resp.json()
+}
--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -8,7 +8,7 @@ import About from "./components/About/About.jsx";
 import Home from "./components/Home/Home.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import User from "./components/User/User.jsx";
-import Github from "./components/GitHub/Github.jsx";
+import Github, { githubLoader } from "./components/GitHub/Github.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -37,7 +37,10 @@ const router = createBrowserRouter(
       <Route path="about" element = {<About/>}/>
       <Route path="contact" element={<Contact/>}/>
       <Route path="user/:userId" element={<User/>}/>
-      <Route path="github" element={<Github/>}/>
+      <Route 
+      loader={githubLoader}
+      path="github" 
+      element={<Github/>}/>
     </Route>
   )
 )
