bf92d276d5e5e6956467be827b6cd86ae07fbfbc
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-06-14 00:12:14 +0545

    stylinggs

--- 00day5/src/App.jsx
+++ 00day5/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Form from './components/Form'
+import Thankyou from './components/Thankyou'
 
 function App() {
   return (
+    <>
     <div className='container'>
-      
+      <h1>Feedback Form</h1>
+      <Form/>
+      <Thankyou/>
     </div>
+    </>
   )
 }
 
--- 00day5/src/components/Form.jsx
+++ 00day5/src/components/Form.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Form() {
   return (
-    <div>Form</div>
+    <>
+    <form>
+    <div className='name'>
+      <label htmlFor="name">Name:</label>
+      <input type="text" name="" id="name" />
+    </div>
+    <div className='comment'>
+      <label htmlFor="comment">Comment</label>
+      <textarea name="" id="" rows={8} />
+    </div>
+    <div>
+      <label htmlFor="Mood">Comment</label>
+    </div>
+    <button>Submit</button>
+    </form>
+    </>
   )
 }
 
