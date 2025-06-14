912798c0e871242fa050a3df00531e3dbc2eb155
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-06-14 23:20:07 +0545

    stylibg form component

--- 00day5/src/components/Form.jsx
+++ 00day5/src/components/Form.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
   return (
     <>
-    <form>
-    <div className='name'>
-      <label htmlFor="name">Name:</label>
-      <input type="text" name="" id="name" />
-    </div>
-    <div className='comment'>
-      <label htmlFor="comment">Comment</label>
-      <textarea name="" id="" rows={8} />
-    </div>
-    <div>
-      <label htmlFor="Mood">Comment</label>
-    </div>
-    <button>Submit</button>
-    </form>
+      <form className="form">
+        <div className="name">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="" id="name" />
+        </div>
+        <div className="comment">
+          <label htmlFor="comment">Comment</label>
+          <textarea name="" id="comment" rows={8} />
+        </div>
+        <div className="mood">
+          <label htmlFor="Mood">Mood</label>
+          <select name="" id="">
+            <option value="happy">Happy</option>
+            <option value="sad">Sad</option>
+            <option value="angry">Angry</option>
+          </select>
+        </div>
+        <div className="btn">
+          <button className="btnSub">Submit</button>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default Form
+export default Form;
