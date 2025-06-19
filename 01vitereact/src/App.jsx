fbb056386ff85acfb24c7bfce135d26a3c281237
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-06-19 12:57:48 +0545

    form handeling practice

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -21,7 +21,6 @@
 //   console.log("Updated form state:", form);
 // }, [form]);
 
-
 //   return (
 //     <React.Fragment>
 //       <div>
@@ -53,51 +52,96 @@
 
 // export default App;
 
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   const values ={
+//     name:"",
+//     email:"",
+//     number:"",
+//     address:"",
+//     status:""
+//   }
+
+//   const [form, setForm] = useState(values)
+
+//   const handleChange = (e) => {
+//     const {name,value} = e.target
+//     setForm((prevValue) => ({...prevValue, [name]:value}))
+
+//   }
+//   const submitedHandle = (e) => {
+//     e.preventDefault()
+//     console.log(form)
+//     setForm(values)
+//   }
+//   return (
+//     <form >
+//       <label htmlFor="">Name:</label>
+//       <input name='name' onChange={handleChange} value={form.name} type="text" />
+
+//       <label htmlFor="">Email:</label>
+//       <input name='email' onChange={handleChange} value={form.email} type="text" />
+
+//       <label htmlFor="">Phone Number:</label>
+//       <input name='number' onChange={handleChange} value={form.number} type="text" />
+
+//       <label htmlFor="">Address:</label>
+//       <input name='address' onChange={handleChange} value={form.address}  type="text" />
+
+//       <label htmlFor="">Marital Status:</label>
+//       <input name='status' onChange={handleChange} value={form.status} type="text" />
+
+//       <button onClick={submitedHandle}>Submit</button>
+//     </form>
+//   )
+// }
+
+// export default App
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const initialValue = {
+//     name: "",
+//     email: "",
+//     number: "",
+//     address: "",
+//   };
+//   const [form, setForm] = useState(initialValue);
 
 
-import React, { useState } from 'react'
+//   const handleForm = (e) => {
+//     const{name,value} = e.target
+//     setForm((prevValue) => ({...prevValue,[name]:value}))
+//   }
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+//     console.log(form)
+//     setForm(initialValue)
+//   }
 
-const App = () => {
-  const values ={
-    name:"",
-    email:"",
-    number:"",
-    address:"",
-    status:""
-  }
+//   return (
+//     <form>
+//       <div>
+//         <label htmlFor="">Name:</label>
+//         <input name="name" type="text" value={form.name} onChange={handleForm}/>
+//       </div>
+//       <div>
+//         <label htmlFor="">Email:</label>
+//         <input name="email" type="text" value={form.email} onChange={handleForm}/>
+//       </div>
+//       <div>
+//         <label htmlFor="">Phone Number:</label>
+//         <input name="number" type="text" value={form.number} onChange={handleForm}/>
+//       </div>
+//       <div>
+//         <label htmlFor="">Address:</label>
+//         <input name="address" type="text" value={form.address} onChange={handleForm}/>
+//       </div>
+//       <button type="submit" onClick={handleSubmit}>submit</button>
+//     </form>
+//   );
+// }
 
-  const [form, setForm] = useState(values)
-
-  const handleChange = (e) => {
-    const {name,value} = e.target
-    setForm((prevValue) => ({...prevValue, [name]:value})) 
-  
-  }
-  const submitedHandle = (e) => {
-    e.preventDefault()
-    console.log(form)
-    setForm(values)
-  }
-  return (
-    <form >
-      <label htmlFor="">Name:</label>
-      <input name='name' onChange={handleChange} value={form.name} type="text" />
-
-      <label htmlFor="">Email:</label>
-      <input name='email' onChange={handleChange} value={form.email} type="text" />
-
-      <label htmlFor="">Phone Number:</label>
-      <input name='number' onChange={handleChange} value={form.number} type="text" />
-
-      <label htmlFor="">Address:</label>
-      <input name='address' onChange={handleChange} value={form.address}  type="text" />
-
-      <label htmlFor="">Marital Status:</label>
-      <input name='status' onChange={handleChange} value={form.status} type="text" />
-
-      <button onClick={submitedHandle}>Submit</button>
-    </form>
-  )
-}
-
-export default App
+// export default App;
