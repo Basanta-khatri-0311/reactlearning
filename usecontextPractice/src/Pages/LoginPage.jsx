d5e3665e067ae4eafa38e462396c9f8e32d55c39
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-16 23:48:45 +0545

    Login page with simple auth functionality

--- usecontextPractice/src/Pages/LoginPage.jsx
+++ usecontextPractice/src/Pages/LoginPage.jsx
@@ -35,7 +35,7 @@ const LoginPage = () => {
             value={userName}
             type="text"
             id="username"
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-400"
             placeholder="Enter your username"
           />
         </div>
