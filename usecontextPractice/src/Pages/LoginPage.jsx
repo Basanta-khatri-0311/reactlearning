65c40ee9d652f37bd682655959bb0f4146188dd5
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-17 23:45:31 +0545

    update on task

--- usecontextPractice/src/Pages/LoginPage.jsx
+++ usecontextPractice/src/Pages/LoginPage.jsx
@@ -37,6 +37,7 @@ const LoginPage = () => {
             id="username"
             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-400"
             placeholder="Enter your username"
+            
           />
         </div>
 
